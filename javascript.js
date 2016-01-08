c37e6068e1755cc29d661e5d912b012e88befb55
Lizzy Hamaoka <lizzy161@stmaryspdx.net>
2016-01-08 10:29:32 -0800

    This is my first push to Github

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// javascript stuff goes here
+console.log("please work properly")
+
+var myimgobj =document.images["newimage"];
+function changeImage()
+{
+	document.images["newimage"].src = "https://s3.amazonaws.com/rapgenius/freshprince24_337x233_081720071243.jpg";
+	return true;
+	 
+}
